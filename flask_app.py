b5e4754027e4cf76cd91b9641d984de9d1f01453
CodeWizzard201 <thelionheart201@gmail.com>
2023-12-16 23:17:27 +0000

    Replaced dummy comments with some in memory storage

--- flask_app.py
+++ flask_app.py
@@ -1,13 +1,19 @@
 
-# A very simple Flask Hello World app for you to get started with...
+# Initiative Tracker Web App
+# By Gabe Williams
+# GitHub: CodeWizzard201
 
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
+comments = []
 app.config["DEBUG"] = True
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=["GET", "POST"]) # Accepts whichever methods you list, so GET and POST in this case
 def index():
-    return render_template('index.html', title='Home')
+    if request.method == "GET":
+        return render_template ("index.html", comments=comments)
+
+    comments.append(request.form["contents"])# Extracts the text from the form given the name from the HTML
+    return redirect(url_for('index'))
 
