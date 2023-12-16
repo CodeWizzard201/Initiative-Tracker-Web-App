08a5f113fdb02a46a477fd3efa3292a6599cba04
CodeWizzard201 <thelionheart201@gmail.com>
2023-12-16 21:09:59 +0000

    First commit of the Initiative Tracker web app, with a .gitignore file

---
+++ flask_app.py
@@ -0,0 +1,13 @@
+
+# A very simple Flask Hello World app for you to get started with...
+
+from flask import render_template
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html', title='Home')
+
