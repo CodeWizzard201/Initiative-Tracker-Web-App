02f6c1dfced1f5ab94ce692e6f9222f6407c33b4
CodeWizzard201 <thelionheart201@gmail.com>
2023-12-16 21:20:44 +0000

    Added first version of template

--- flask_app.py
+++ flask_app.py
@@ -1,10 +1,10 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import render_template
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config["DEBUG"] = True
 
 @app.route('/')
 @app.route('/index')
