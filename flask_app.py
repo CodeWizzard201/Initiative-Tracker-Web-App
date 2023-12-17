f6a41819058ba3f03b6f6286826700e7485da658
CodeWizzard201 <thelionheart201@gmail.com>
2023-12-17 04:32:53 +0000

    Added Login Page

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,5 @@
 
-# Initiative Tracker Web App
-# By Gabe Williams
-# GitHub: CodeWizzard201
+# Tutorial Program
 
 from flask import Flask, redirect, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
@@ -38,3 +36,6 @@ def index():
     db.session.commit()
     return redirect(url_for('index'))
 
+@app.route("/login/")
+def login():
+    return render_template("login_page.html")
