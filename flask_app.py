79d8f83750a0ef979787af803c33f39b77ba20b1
CodeWizzard201 <thelionheart201@gmail.com>
2023-12-22 23:03:33 +0000

    Fixed sorting, added signs

--- flask_app.py
+++ flask_app.py
@@ -2,12 +2,18 @@
 # By Gabe Williams
 # Tutorial Program
 
-from flask import Flask, render_template
-from processing import Combatant, make_combatant
+from flask import Flask, redirect, render_template, request, url_for
+from processing import make_combatant, sort_combatants
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-@app.route('/')
+combatants = []
+
+@app.route('/', methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    if request.method == "GET":
+        return render_template("index.html", combatants=combatants)
+    combatants.append(make_combatant(request.form["name"], request.form["modifier"], request.form["roll"]))
+    sort_combatants(combatants, True, True)
+    return redirect(url_for('index'))
--- processing.py
+++ processing.py
@@ -1,3 +1,5 @@
+from functools import lru_cache
+
 # Where the processing of the data takes place
 class Combatant:
     def __init__(self, name, mod, roll):
@@ -5,13 +7,18 @@ class Combatant:
         self.mod = mod
         self.roll = roll
     # Calculates the Sign of the Number based on if it's positive or negative
-    def get_sign_(self):
-        if self.mod > 0:
+    @property
+    def sign(self):
+        return self._sign(self.mod)
+
+    # Cache the value from the computation of the mod, if it changes then it will store the new result in the cache, otherwise it will spit out the original value
+    @lru_cache
+    def _sign(self, mod):
+        if int(self.mod) >= 0:
             return "+"
-        elif self.mod < 0:
-            return "-"
         else:
             return ""
+
     # Getters
     def get_name(self):
         return self.name
@@ -32,6 +39,20 @@ class Combatant:
 
     def set_roll(self, value):
         self.roll = value
+
+def sort_combatants(array, descending, tiebreaker):
+    if descending and tiebreaker:
+        #Do descending sorting with tiebreaker
+        array.sort(key=lambda x:(x.roll, x.mod), reverse=True)
+    elif descending:
+        #Do descending sorting with no tiebreaker
+        array.sort(key=lambda x:x.roll, reverse=True)
+    elif tiebreaker:
+        #Do ascending sorting with tiebreaker
+        array.sort(key=lambda x:(x.roll, x.mod))
+    else:
+        #Do ascending sorting with no tiebreaker
+        array.sort(key=lambda x:x.roll)
 
 def make_combatant(name, mod, roll):
     return Combatant(name, mod, roll)
